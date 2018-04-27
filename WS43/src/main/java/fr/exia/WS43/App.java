3719f06dad852a82ae5bebf54d9988c502dedcd2
Mmouky <33157839+Mmouky@users.noreply.github.com>
2018-04-27 09:07:53 +0200

    Phase 1

---
+++ WS43/src/main/java/fr/exia/WS43/App.java
@@ -0,0 +1,13 @@
+package fr.exia.WS43;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
